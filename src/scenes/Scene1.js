c9564d80d2ee0567516841778f46502ad00c1247
JoakimSjogren <70525815+JoakimSjogren@users.noreply.github.com>
2021-05-12 12:34:57 +0200

    Improved player movement and collision

--- src/scenes/Scene1.js
+++ src/scenes/Scene1.js
@@ -1,7 +1,5 @@
 import Phaser from 'phaser';
 let house;
-let prevX;
-let prevY;
 class Scene1 extends Phaser.Scene {
   constructor() {
     super({ key: 'Scene1' });
@@ -14,7 +12,7 @@ class Scene1 extends Phaser.Scene {
   }
 
   create() {
-    this.speed = 2;
+    this.speed = 250;
     this.add.tileSprite(400, 300, 800 * 2, 600 * 2, 'background').setScale(0.5);
 
     house = this.physics.add.image(400, 600, 'house').setScale(0.7);
@@ -23,7 +21,6 @@ class Scene1 extends Phaser.Scene {
     
     // this.player = this.add.rectangle(200,200,20,20, 0x000);
     this.player = this.add.circle(200, 200, 20, 0x000);
-
     this.physics.add.existing(this.player);
     this.player.body.setCollideWorldBounds(true);
 
@@ -36,37 +33,30 @@ class Scene1 extends Phaser.Scene {
   }
 
   update(delta) {
+    let xVelocity = 0;
+    let yVelocity = 0;
+
     if (this.key_D.isDown) {
-      this.player.x += this.speed;
+    xVelocity += 1;
     }
     if (this.key_A.isDown) {
-      this.player.x -= this.speed;
+      xVelocity += -1;
     }
     if (this.key_W.isDown) {
-      this.player.y -= this.speed;
+      yVelocity += -1;
     }
     if (this.key_S.isDown) {
-      this.player.y += this.speed;
+      yVelocity += 1;
     }
+
+    // Adds velocity to player
+    this.player.body.setVelocity(xVelocity*this.speed, yVelocity*this.speed);
 
     this.input.on('pointermove', function (pointer) {
       let cursor = pointer;
       let angle = Phaser.Math.Angle.Between(this.player.x, this.player.y, cursor.x + this.cameras.main.scrollX, cursor.y + this.cameras.main.scrollY)
       this.player.rotation = angle;
     }, this);
-
-    // Collision between player and house
-   if (
-     this.player.x + this.player.width / 2 > (house.x - (house.width*0.7) /2) &&
-     this.player.x - this.player.width / 2 < (house.x + (house.width*0.7) /2) &&
-     this.player.y + this.player.height / 2 > (house.y - (house.height*0.7) /2) &&
-     this.player.y - this.player.height / 2 < (house.y + (house.height*0.7) /2)
-     ){
-       this.player.x = prevX;
-       this.player.y = prevY;
-    } 
-    prevX = this.player.x;
-    prevY = this.player.y;
   }
 }
 
