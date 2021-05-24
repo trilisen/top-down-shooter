2a712d1d10b07d88e937b50aa5e1d12fe6132e11
JoakimSjogren <70525815+JoakimSjogren@users.noreply.github.com>
2021-05-24 16:14:21 +0200

    Changed house, and zombie movement
    You can now walk into the house, zombies now tries to get into the house

--- src/scenes/zombie.js
+++ src/scenes/zombie.js
@@ -1,15 +1,28 @@
+var door = new Phaser.Math.Vector2();
 class Zombie {
   constructor(scene, house, speed, size, health, color) {
     this.atHouse = false;
+    this.movingToDoor = true;
     this.scene = scene;
     this.house = house;
     this.health = health;
     this.speed = speed;
+    this.size = size;
+    door.x = 400;
+    door.y = 400 - (30*this.size);
+
     const xPos = Math.floor(Math.random() * 1200) - 120;
     const yPos = -Math.floor(Math.random() * 300) - 25;
     this.zombie = this.scene.add.circle(xPos, yPos, 20, color).setScale(size);
     this.scene.physics.add.existing(this.zombie);
-    this.scene.physics.add.collider(this.house.house, this.zombie, () => {
+
+    for (let index = 1; index < this.house.house.length; index++) {
+      this.scene.physics.add.collider(this.house.house[index], this.zombie);
+    }
+
+    
+
+    this.scene.physics.add.collider(this.house.house[0], this.zombie, () => {
       this.atHouse = true;
       this.zombie.body.setVelocity(0, 0);
     });
@@ -17,10 +30,22 @@ class Zombie {
   }
   update() {
     if (this.dead) return;
-    if (!this.atHouse) {
+
+
+    if (this.movingToDoor) {
       this.scene.physics.moveToObject(
         this.zombie,
-        this.house.house,
+        door,
+        this.speed
+      );
+      if (Math.abs((this.zombie.body.x + (30 * this.size)) - door.x) < 30) {
+        this.movingToDoor = false;
+      }
+    }
+    else if (!this.atHouse) {
+      this.scene.physics.moveToObject(
+        this.zombie,
+        this.house.house[0],
         this.speed
       );
     } else {
